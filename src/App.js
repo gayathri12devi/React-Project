44155f0edf05554bfd36517f74c11c2497d86e9c
Moha Gayathri <mohagayathrik@gmail.com>
2026-05-21 04:42:31 +0000

    added logic for taking turns in game

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import { useState } from "react";
 
-function Square() {
-    const [val,setVal] = useState(null);
-    function handleClick() {
-        setVal('X');
-    }
+function Square({val,onSquareClick}) {
     return (
-        <button className="square" onClick={handleClick}>{val}</button>
+        <button className="square" onClick={onSquareClick}>{val}</button>
     );
 }
 
 export default function Board() {
+    const [xIsNext,setXIsNext] = useState(true);
+    const [squares,setSquares] = useState(Array(9).fill(null));
+    function handleClick(i) {
+        const nextSquares = squares.slice();
+        if(xIsNext) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
+        setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
     return (
         <>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square val={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square val={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square val={squares[2]} onSquareClick={() => handleClick(2)}/>
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square val={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square val={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square val={squares[5]} onSquareClick={() => handleClick(5)}/>
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square val={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square val={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square val={squares[8]} onSquareClick={() => handleClick(8)}/>
             </div>
         </>
     );
